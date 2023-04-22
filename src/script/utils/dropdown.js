8ce44e130bbe1cf13372a3cd1e04ebb716768218
Stv-devl <lansadstevan@gmail.com>
2023-04-22 17:03:44 +0700

    fix js and work on Sass

--- src/script/utils/dropdown.js
+++ src/script/utils/dropdown.js
@@ -1,35 +1,45 @@
+//filter
+import FilterSearch from "../filter/searchv1.js";
+/*import FilterSearch from "../filter/searchv2.js";*/
+
 //class for open drowpdown
 class DropDown {
   constructor() {
-    this.buttons = document.querySelectorAll(".btn");
+    this.buttons = document.querySelectorAll(".dropdown_input");
+    this.buttonClose = document.querySelectorAll(".btnClose");
   }
   //at click on each button launch closePopup or openPopup
   ingredientDropDown() {
     this.buttons.forEach((button) => {
       button.addEventListener("click", (e) => {
-        const dropdownInput = button.nextElementSibling;
-        const dropdownList = button.parentElement.children[3];
+        const dropdownContainer = button.parentElement.parentElement;
 
-        if (
-          dropdownInput.classList.contains("active") &&
-          dropdownList.classList.contains("active")
-        ) {
-          this.closePopup(dropdownInput, dropdownList);
+        if (!dropdownContainer.classList.contains("active")) {
+          this.openPopup(dropdownContainer);
+        }
+        return;
+      });
+    });
+    this.buttonClose.forEach((button) => {
+      button.addEventListener("click", (e) => {
+        const dropdownContainer = button.parentElement.parentElement;
+
+        if (dropdownContainer.classList.contains("active")) {
+          this.closePopup(dropdownContainer);
         } else {
-          this.openPopup(dropdownInput, dropdownList);
+          this.openPopup(dropdownContainer);
         }
       });
     });
   }
-  //when we open the popup the dropwdown search input and the drowdown ingredient list will be active. Launch getData
-  openPopup(dropdownInput, dropdownList) {
-    dropdownInput.classList.add("active");
-    dropdownList.classList.add("active");
+  //when we open the popup the dropdown_container will be active.
+  openPopup(dropdownContainer) {
+    dropdownContainer.classList.add("active");
   }
-  //when we close the popup the dropwdown search input and the drowdown ingredient we remove active.Launch getData
-  closePopup(dropdownInput, dropdownList) {
-    dropdownInput.classList.remove("active");
-    dropdownList.classList.remove("active");
+  //when we close the popup the dropdown_container will be active.
+  closePopup(dropdownContainer) {
+    console.log("ça ferme");
+    dropdownContainer.classList.remove("active");
   }
 }
 
